43750fceac2c49bbe75dc9f88c675a26ca0f6c9c
cad-san <cadsan1986@gmail.com>
2013-09-01 21:16:58 +0900

    change base class of MockObstacle Obstacle => SquareObstacle

--- mocks/world/MockObstacle.h
+++ mocks/world/MockObstacle.h
@@ -1,37 +1,22 @@
 #ifndef D_MOCK_OBSTACLE_H
 #define D_MOCK_OBSTACLE_H
 
-#include "Obstacle.h"
+#include "SquareObstacle.h"
 #include "CppUTestExt/MockSupport.h"
 
-class MockObstacle : public Obstacle
+class MockObstacle : public SquareObstacle
 {
 private:
     int dummy_dist;
 
 public:
     MockObstacle(int x, int y, int width, int height)
-        : Obstacle(x, y, width, height)
+        : SquareObstacle(x, y, width, height)
     {
         this->dummy_dist = World::INVALID_DISTANCE;
     }
 
     virtual ~MockObstacle() {};
-
-    virtual bool isInArea(int x, int y) const
-    {
-        return ( this->x <= x && x < this->x + this->width  ) &&
-               ( this->y <= y && y < this->y + this->height );
-    }
-
-    virtual bool isInArea(const Obstacle& obstacle) const
-    {
-        Geo::Point min = getMinPoint();
-        Geo::Point max = getMaxPoint();
-
-        return ( obstacle.isInArea(min.x(), min.y()) || obstacle.isInArea(min.x(), max.y()) ||
-                 obstacle.isInArea(max.x(), min.y()) || obstacle.isInArea(max.x(), max.y()) );
-    }
 
     virtual int getDistance(int x, int y, double angle) const
     {
