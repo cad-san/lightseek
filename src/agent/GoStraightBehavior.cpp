1c8cffcf6e9196353cee51a9f0dd0c0cc3b74193
cad-san <cadsan1986@gmail.com>
2013-09-21 23:23:23 +0900

    add GoStraightBehavior class

---
+++ src/agent/GoStraightBehavior.cpp
@@ -0,0 +1,39 @@
+#include "GoStraightBehavior.h"
+
+static const int THRESHOLD_DIST = 50;
+
+GoStraightBehavior::GoStraightBehavior(const DistSensorPtr& sensor_ptr, const ActionPtr& action_ptr)
+    : sensor(sensor_ptr), action(action_ptr)
+{
+    sensed_dist = DistSensor::INVALID_DISTANCE;
+}
+
+GoStraightBehavior::~GoStraightBehavior()
+{
+}
+
+void GoStraightBehavior::init()
+{
+    sensed_dist = DistSensor::INVALID_DISTANCE;
+}
+
+void GoStraightBehavior::sensing()
+{
+    sensed_dist = sensor->getDistance();
+    sensed_dist = sensed_dist < 100 ? sensed_dist : 100;
+}
+
+void GoStraightBehavior::perform()
+{
+    if(!isActive())
+        return;
+
+    int speed = (sensed_dist - THRESHOLD_DIST) / 2;
+
+    action->moveFront(speed);
+}
+
+const bool GoStraightBehavior::isActive() const
+{
+    return (sensed_dist >= THRESHOLD_DIST);
+}
