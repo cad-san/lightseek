81bb0ec3745b575e5a3eda49941417042327be7f
cad-san <cadsan1986@gmail.com>
2013-12-06 07:02:43 +0900

    add temlepete member func in Geo::Point
    - Geo::Point#x<ret_type>()
    - Geo::Point#y<ret_type>()
    - Geo::Point#length<ret_type>()

--- include/world/Geometry.h
+++ include/world/Geometry.h
@@ -20,6 +20,22 @@ public:
     type y() const;
 
     type length(void) const;
+
+    template <typename ret_type> ret_type x() const
+    {
+        return static_cast<ret_type>( x() );
+    }
+
+    template <typename ret_type> ret_type y() const
+    {
+        return static_cast<ret_type>( y() );
+    }
+
+    template <typename ret_type> ret_type length(void) const
+    {
+        return static_cast<ret_type>( length() );
+    }
+
 };
 
 class Line
